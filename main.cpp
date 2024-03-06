dccfc4ee6fc31ff4bc0280c1d577b4815d81d8fe
Shahir Jagot <jagot9@hotmail.com>
2024-03-06 10:34:41 +0000

    chore - documentation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+/*
+* Course Title					-					MSc Robotics
+* Module						-					Machine Vision for Robotics
+* Assignment					-					Object Measurement 
+* Lecturer						-					Dr. Stuart Barnes
+* Author						-					Shahir Jagot
+* 
+* Project Description			-					Measures dimensions of objects using a webcam 
+*/
+
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/core/utils/logger.hpp>
 #include <iostream>
 
 using namespace std;
@@ -7,6 +18,9 @@ using namespace cv;
 
 int main()
 {
+	//Set logging level to keep console clean of non-pertinent messages
+	utils::logging::setLogLevel(cv::utils::logging::LogLevel::LOG_LEVEL_WARNING);
+
 	// try to open image
 	Mat inputImg = imread("lena.bmp", -1);
 	if (inputImg.empty())
