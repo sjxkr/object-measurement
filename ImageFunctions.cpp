3b244a5a9d009f8710b8d348a1b31b3baa003107
Shahir Jagot <jagot9@hotmail.com>
2024-03-06 11:21:32 +0000

    feat - add image functions hdr and implementation files

---
+++ ImageFunctions.cpp
@@ -0,0 +1,63 @@
+#include "ImageFunctions.h"
+
+/*
+******* Header Template *******
+* Purpose - xxxxxxx
+* Parameters - xxxxxxx
+* Outputs - xxxxxx
+*******************************/
+
+void cameraCalibration()
+{
+	/*
+	* Purpose - xxxxxxx
+	* Parameters - xxxxxxx
+	* Outputs - xxxxxx
+	*/ 
+}
+
+void calibrationCheck()
+{
+	/*
+	* Purpose - xxxxxxx
+	* Parameters - xxxxxxx
+	* Outputs - xxxxxx
+	*/
+
+}
+
+void remapImage()
+{
+	/*
+	* Purpose - xxxxxxx
+	* Parameters - xxxxxxx
+	* Outputs - xxxxxx
+	*/
+}
+
+void edgeDetection()
+{
+	/*
+	* Purpose - xxxxxxx
+	* Parameters - xxxxxxx
+	* Outputs - xxxxxx
+	*/
+}
+
+void shapeRecognition()
+{
+	/*
+	* Purpose - xxxxxxx
+	* Parameters - xxxxxxx
+	* Outputs - xxxxxx
+	*/
+}
+
+void measureObject()
+{
+	/*
+	* Purpose - xxxxxxx
+	* Parameters - xxxxxxx
+	* Outputs - xxxxxx
+	*/
+}
---
+++ ImageFunctions.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+/*************************************************************** 
+Constants 
+****************************************************************/
+
+
+/*************************************************************** 
+Functions 
+****************************************************************/ 
+
+void cameraCalibration();
+void calibrationCheck();
+void remapImage();
+void edgeDetection();
+void shapeRecognition();
+void measureObject();
