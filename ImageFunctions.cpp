467f83f030d2d6045a0ff2944039d5615fe7a2a0
Shahir Jagot <jagot9@hotmail.com>
2024-03-13 14:51:45 +0000

    fix - cal image message box position

--- ImageFunctions.cpp
+++ ImageFunctions.cpp
@@ -91,17 +91,22 @@ void captureCalibrationImages()
 				imwrite(imgPath, rawframe);
 			}
 
-			// Capture complete dialog box
-			int msgBox = MessageBox(NULL, (LPCWSTR)L"Required number of samples for calibration achieved.",
-				(LPCWSTR)L"Capture Complete", MB_ICONINFORMATION | MB_OK);
-
-			// exit while loop
-			switch (msgBox)
+			if (imgNumber == nSamples + 1)
 			{
-			case IDOK:
-				break;
+				// Capture complete dialog box
+				int msgBox = MessageBox(NULL, (LPCWSTR)L"Required number of samples for calibration achieved.\nAcknowledge message and press 'Esc' key.",
+					(LPCWSTR)L"Capture Complete", MB_ICONINFORMATION | MB_OK);
+
+				// exit while loop
+				switch (msgBox)
+				{
+				case IDOK:
+					break;
+				}
 			}
 		}
+
+
 
 	}
 
