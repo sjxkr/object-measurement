ab7cff8d2e5f58574b72432ec4847f3f4d3ddfe1
sjxkr <jagot9@hotmail.com>
2024-03-18 09:56:18 +0000

    test - increase canny threshold lower by 0.1

--- ImageFunctions.cpp
+++ ImageFunctions.cpp
@@ -425,7 +425,7 @@ Mat edgeDetection(Mat& image)
 
 	// determine thresholds & binarize image
 	CannyThreshMax = threshold(imgGray, imgGrayThresh, 0, 255, THRESH_BINARY | THRESH_OTSU);
-	CannyThreshMin = 0.1 * CannyThreshMax;
+	CannyThreshMin = 0.2 * CannyThreshMax;
 
 	// print threshold values
 	cout << "Canny Edge Detection Threshold Upper = " << CannyThreshMax << endl;
