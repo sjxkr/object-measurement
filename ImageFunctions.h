37dc4485ae8dbf379cacfe2379622a383c6fc326
sjxkr <jagot9@hotmail.com>
2024-03-19 07:23:31 +0000

    test - using 5p coin as reference

--- ImageFunctions.h
+++ ImageFunctions.h
@@ -31,7 +31,7 @@ const char newline('\n');
 const char terminator('@');
 const char nullChar('\0');
 const string calFilename("Calibration.bin");		// calibration file
-const double realObjWidth = 50.0;					// real object width for pixel to mm conversion
+const double realObjWidth = 18.0;					// real object width for pixel to mm conversion
 const int drawLineThickness = 2;						// line thickness for drawing shapes
 const int fThickness = 1;							// font thickness when required
 
