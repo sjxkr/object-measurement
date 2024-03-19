0e0247fd5277820d4e20548b4202845f154cc9ed
sjxkr <jagot9@hotmail.com>
2024-03-19 08:11:09 +0000

    test - epsilon to 0.1 of arc length

--- ImageFunctions.cpp
+++ ImageFunctions.cpp
@@ -523,7 +523,7 @@ void shapeRecognition()
 		shapeAreas.push_back(area);
 
 		// Approximate the shape
-		double epsilon = 0.01 * arcLength(filteredContours[i], true);		// smaller epsilon = more points in approximation
+		double epsilon = 0.1 * arcLength(filteredContours[i], true);		// smaller epsilon = more points in approximation
 		vector<Point> approx;
 		approxPolyDP(filteredContours[i], approx, epsilon, true);
 
